8e749b76861e27d6dbd28bd08ea3349c48dbaab1
Giulio998 <giulio_favia@hotmail.it>
2024-04-13 15:46:08 +0200

    Reactive server and API call

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 import AppNavBar from "./components/AppNavBar.vue"
 import AppCard from "./components/AppCard.vue"
 import AppFooter from "./components/AppFooter.vue"
-import dbJson from "./db.json"
+import { store } from "./store"
+import axios from "axios"
+
 
 export default {
   components: {
@@ -12,8 +14,19 @@ export default {
   },
   data() {
     return {
-      dbJson
+      store
     }
+  },
+  methods: {
+    getProducts(){
+      axios.get("http://localhost:3000/products").then((res)=>{
+        this.store.productsArray = res.data;
+        
+      })
+    }
+  },
+  mounted() {
+    this.getProducts()
   },
   
 
@@ -27,7 +40,7 @@ export default {
   <main class="mainPadding">
     <div class="container">
       <div class="row prodotti">
-        <template v-for="card in dbJson.products" >
+        <template v-for="card in store.productsArray" >
           <AppCard :brand="card.brand" :frontImage="card.frontImage" :backImage="card.backImage" :price="card.price" :name="card.name" :badges="card.badges" :isInFavorites="card.isInFavorites"/>
         </template>
 
